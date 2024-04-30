10832308623ce77ce6dbdf40069fd9d6fc6c4803
Viktoria Moskalenko <vicky1234555617@gmail.com>
2024-04-30 14:26:00 +0100

    initial commit

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,3 +1,4 @@
+using eTickets.Data;
 using eTickets.Models;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
@@ -7,10 +8,12 @@ namespace eTickets.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly AppDbContext _context;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, AppDbContext context)
         {
             _logger = logger;
+            _context = context;
         }
 
         public IActionResult Index()
